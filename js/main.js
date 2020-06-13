4d786ce2eaac7c66ab265fc0cf2e4a5ae7a5ab87
angeloesaki <angeloesaki@gmail.com>
2020-06-13 18:03:17 +0900

    クイズデータの用意

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 "use strict";
 {
+  const question = document.getElementById("question");
+  const choices = document.getElementById("choices");
+  const button = document.getElementById("button");
+
+  const quizSet = [
+    { q: "Which one is A?", c: ["A", "B", "C"] },
+    { q: "Which one is D?", c: ["D", "E", "F"] },
+    { q: "Which one is G?", c: ["H", "I", "J"] },
+  ];
+
+  let currentNum = 0;
 }
