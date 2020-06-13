af875628e95404135317bbde6839eb87a9c2024d
angeloesaki <angeloesaki@gmail.com>
2020-06-13 18:38:52 +0900

    JavaScriptで問題文と選択肢を表示

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@
     { q: "Which one is D?", c: ["D", "E", "F"] },
     { q: "Which one is G?", c: ["H", "I", "J"] },
   ];
+  let currentNum = 0; //現在の問題番号
 
-  let currentNum = 0;
+  question.textContent = quizSet[currentNum].q; //現在の問題番号の問題文を代入
+
+  // 現在の問題文の選択肢を表示
+  quizSet[currentNum].c.forEach((choice) => {
+    const li = document.createElement("li");
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
 }
