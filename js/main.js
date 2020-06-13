3c63c596068a22c35e7bb2e0642ce5626651e254
angeloesaki <angeloesaki@gmail.com>
2020-06-13 21:55:46 +0900

    shuffle関数を定義

--- js/main.js
+++ js/main.js
@@ -9,9 +9,25 @@
     { q: "Which one is D?", c: ["D", "E", "F"] },
     { q: "Which one is G?", c: ["H", "I", "J"] },
   ];
-  let currentNum = 0; //現在の問題番号
+  //現在の問題番号
+  let currentNum = 0;
 
-  question.textContent = quizSet[currentNum].q; //現在の問題番号の問題文を代入
+  //現在の問題番号の問題文を代入
+  question.textContent = quizSet[currentNum].q;
+
+  function shuffle(array) {
+    //フィッシャー・イェーツシャッフルのアルゴリズム
+    //for文の引数はランダムに選ぶ範囲の終点のインデックス
+    for (let i = array.length - 1; i > 0; i--) {
+      //上記の範囲の中からランダムに選ぶ要素のインデックス
+      //1巡目は0,1,2のいずれかの数字が代入される
+      //2巡目は0,1
+      const j = Math.floor(Math.random() * (i + 1));
+      //分割代入
+      [array[j], array[i]] = [array[i], array[j]];
+    }
+    return array;
+  }
 
   // 現在の問題文の選択肢を表示
   quizSet[currentNum].c.forEach((choice) => {
